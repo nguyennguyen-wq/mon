630bff86b0cfdae3e7b3f5e228e9ffbdc0ee1bd0
nguyennguyen-wq <nguyen_ng@hotmail.com>
2022-05-30 17:58:23 +0200

    commit n

---
+++ Tables/dbo.Kontingents.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Kontingents] (
+  [KontintId] [int] IDENTITY,
+  [Name] [nvarchar](50) NULL,
+  CONSTRAINT [PK_Kontingents] PRIMARY KEY CLUSTERED ([KontintId])
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo.Medlems.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Medlems] (
+  [Medlem_Id] [uniqueidentifier] NOT NULL,
+  [Fornavn] [nvarchar](max) NULL,
+  [Etternavn] [nvarchar](max) NULL,
+  [Bosted] [nvarchar](max) NULL,
+  [MobilTlf] [int] NOT NULL,
+  [Email] [nvarchar](max) NULL,
+  [Fodselsdato] [datetime2] NOT NULL,
+  [CurrentKontintId] [int] NOT NULL,
+  CONSTRAINT [PK_Medlems] PRIMARY KEY CLUSTERED ([Medlem_Id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE INDEX [IX_Medlems_CurrentKontintId]
+  ON [dbo].[Medlems] ([CurrentKontintId])
+  ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Medlems]
+  ADD CONSTRAINT [FK_Medlems_Kontingents_CurrentKontintId] FOREIGN KEY ([CurrentKontintId]) REFERENCES [dbo].[Kontingents] ([KontintId]) ON DELETE CASCADE
+GO
