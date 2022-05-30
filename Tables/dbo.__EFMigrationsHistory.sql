067055ef3ab225df5c7d72a96302aa33a5896d0b
nguyennguyen-wq <104008925+nguyennguyen-wq@users.noreply.github.com>
2022-05-30 19:18:17 +0200

    Delete dbo.__EFMigrationsHistory.sql

--- Tables/dbo.__EFMigrationsHistory.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[__EFMigrationsHistory] (
-  [MigrationId] [nvarchar](150) NOT NULL,
-  [ProductVersion] [nvarchar](32) NOT NULL,
-  CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY CLUSTERED ([MigrationId])
-)
-ON [PRIMARY]
-GO
