73c545d61ce360ad9a9821d8358121e9df32e242
nguyennguyen-wq <104008925+nguyennguyen-wq@users.noreply.github.com>
2022-05-30 19:17:38 +0200

    Delete dbo.Kontingents.sql

--- Tables/dbo.Kontingents.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Kontingents] (
-  [KontintId] [int] IDENTITY,
-  [Name] [nvarchar](50) NULL,
-  CONSTRAINT [PK_Kontingents] PRIMARY KEY CLUSTERED ([KontintId])
-)
-ON [PRIMARY]
-GO
